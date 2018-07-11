521c8838ccccf9117b49549d024e11bbb77b020e
geoSkogen <borninthecaul@gmail.com>
2018-07-11 13:15:23 -0700

    fixes keyed entry

---
+++ test.js
@@ -0,0 +1,13 @@
+'use strict'
+function cleanInputString(badinput) {
+  var goodinput = ""
+  var badarray = badinput.split("")
+  var len = badarray.length
+
+  for (let i = 0; i < len; i++) {
+    if (!isNaN(badarray[i])) {
+      goodinput += badarray[i]
+    }
+  }
+  return goodinput
+}
